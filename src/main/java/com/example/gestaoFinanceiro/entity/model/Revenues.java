16a17e95b818ed175c7c69d006e938dea177af32
kaua <kaua.santanaj@gmail.com>
2026-01-07 15:13:36 -0300

    add model database revenues

---
+++ src/main/java/com/example/gestaoFinanceiro/entity/model/Revenues.java
@@ -0,0 +1,41 @@
+package com.example.gestaoFinanceiro.entity.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name="revenues")
+public class Revenues {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(name="value", nullable = false)
+    private BigDecimal value;
+
+
+    @Column(name="date", nullable = false)
+    private LocalDate date;
+
+
+    @Column(name="description", nullable = false)
+    private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = true)
+    private Category category;
+
+
+
+
+}
