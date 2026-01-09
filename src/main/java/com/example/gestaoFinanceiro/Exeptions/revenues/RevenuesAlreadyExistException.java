1e009e5b5b989cfada6f55fe599735c7e6f6c317
kaua <kaua.santanaj@gmail.com>
2026-01-09 14:40:26 -0300

    add exception conflict

--- src/main/java/com/example/gestaoFinanceiro/Exeptions/revenues/RevenuesAlreadyExistException.java
+++ src/main/java/com/example/gestaoFinanceiro/Exeptions/revenues/RevenuesAlreadyExistException.java
@@ -1,5 +1,10 @@
 package com.example.gestaoFinanceiro.Exeptions.revenues;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class RevenuesAlreadyExistException extends RuntimeException {
     public RevenuesAlreadyExistException() {
         super("Revenues already Exist");
