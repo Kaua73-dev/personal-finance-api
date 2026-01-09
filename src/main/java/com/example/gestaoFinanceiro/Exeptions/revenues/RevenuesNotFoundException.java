a83870f2126ea71ba51f2914289955cd8566058f
kaua <kaua.santanaj@gmail.com>
2026-01-09 14:40:56 -0300

    add exception conflict

--- src/main/java/com/example/gestaoFinanceiro/Exeptions/revenues/RevenuesNotFoundException.java
+++ src/main/java/com/example/gestaoFinanceiro/Exeptions/revenues/RevenuesNotFoundException.java
@@ -1,5 +1,10 @@
 package com.example.gestaoFinanceiro.Exeptions.revenues;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RevenuesNotFoundException extends RuntimeException {
     public RevenuesNotFoundException() {
         super("Revenues not found");
