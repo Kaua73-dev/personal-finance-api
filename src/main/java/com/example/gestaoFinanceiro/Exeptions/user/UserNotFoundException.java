e18ffcc4f82cbb2d734c2476b5d34d16138ad772
kaua <kaua.santanaj@gmail.com>
2026-01-09 14:39:40 -0300

    add exception not found

--- src/main/java/com/example/gestaoFinanceiro/Exeptions/user/UserNotFoundException.java
+++ src/main/java/com/example/gestaoFinanceiro/Exeptions/user/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.gestaoFinanceiro.Exeptions.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
         super("user not found");
