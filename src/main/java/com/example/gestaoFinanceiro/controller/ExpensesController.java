c4832a7f6ca552e10455fda0b4b5854e5094da76
kaua <kaua.santanaj@gmail.com>
2026-01-09 15:41:43 -0300

    add postMapping for createExpenses

--- src/main/java/com/example/gestaoFinanceiro/controller/ExpensesController.java
+++ src/main/java/com/example/gestaoFinanceiro/controller/ExpensesController.java
@@ -2,7 +2,11 @@ package com.example.gestaoFinanceiro.controller;
 
 
 
+import com.example.gestaoFinanceiro.dto.request.ExpensesRequest;
+import com.example.gestaoFinanceiro.dto.response.ExpensesResponse;
 import com.example.gestaoFinanceiro.service.ExpensesService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +23,10 @@ public class ExpensesController {
     }
 
 
-
-
+    @PostMapping("/expenses")
+    public ExpensesResponse createExpenses(@RequestBody ExpensesRequest request){
+        return expensesService.createExpenses(request);
+    }
 
 
 }
