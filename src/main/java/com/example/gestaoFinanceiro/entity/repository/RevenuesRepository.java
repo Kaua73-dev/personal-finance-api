8340af96e68666f04cd70173245afa5268c03016
kaua <kaua.santanaj@gmail.com>
2026-01-10 12:37:53 -0300

    add response to categoryTotalResponse

--- src/main/java/com/example/gestaoFinanceiro/entity/repository/RevenuesRepository.java
+++ src/main/java/com/example/gestaoFinanceiro/entity/repository/RevenuesRepository.java
@@ -3,7 +3,9 @@ package com.example.gestaoFinanceiro.entity.repository;
 import com.example.gestaoFinanceiro.entity.model.Revenues;
 import com.example.gestaoFinanceiro.entity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -18,5 +20,23 @@ public interface RevenuesRepository extends JpaRepository<Revenues, Integer> {
     List<Revenues> findByUserAndDateBetween(User user, LocalDate start, LocalDate end);
 
     Optional<Revenues> findByUserAndNameCategory(User user, String nameCategory);
+
+
+
+    @Query("SELECT COALASCE(SUM(r.value), 0) FROM Revenues r WHERE r.user = :user")
+    BigDecimal totalRevenuesByUser(User user);
+
+
+    @Query("""
+        SELECT new com.example.gestaoFinanceiro.dto.response.CategoryTotalResponse(
+            r.nameCategory,
+            SUM(r.value)
+        )
+        FROM Revenues r
+        WHERE r.user = :user
+        GROUP BY r.nameCategory
+    """)
+
+
 
 }
