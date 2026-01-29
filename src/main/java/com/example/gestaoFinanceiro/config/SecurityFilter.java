fbc82d3f7ecac0a5b172a6753ad3f54b4e0b5503
kaua <kaua.santanaj@gmail.com>
2026-01-29 14:57:27 -0300

    ability routes login and register

--- src/main/java/com/example/gestaoFinanceiro/config/SecurityFilter.java
+++ src/main/java/com/example/gestaoFinanceiro/config/SecurityFilter.java
@@ -27,6 +27,13 @@ public class SecurityFilter extends OncePerRequestFilter {
     UserRepository userRepository;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getServletPath();
+
+        return path.equals("/auth/login") || path.equals("/auth/register");
+    }
+    
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
